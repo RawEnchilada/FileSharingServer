bd2fe6392652d1af659e015c333ab1c5341d17dd
RawEnchilada <wittmann.aron@gmail.com>
2022-03-15 13:55:32 +0100

    more information

--- start.bat
+++ start.bat
@@ -1,6 +1,13 @@
+@echo off
+
 if not exist node_modules\ (
     echo "Installing dependencies..."
     npm install
+)
+if not exist config.json (
+    echo "Please create a config.json file based on the example given."
+    pause
+    exit
 )
 
 node index.js
--- start.sh
+++ start.sh
@@ -5,4 +5,10 @@ if ! [ -d "node_modules" ]; then
     npm install
 fi
 
+if ! [ -f "config.json" ]; then
+    echo "Please create a config.json file based on the example given.";
+    read -n 1;
+    exit
+fi
+
 node index.js
